3bf5040af476429be1bdc9f36ca1ed731548875a
j95081456 <61644544+j95081456@users.noreply.github.com>
2025-10-03 18:57:07 +0800

    Update replace.pl

--- replace.pl
+++ replace.pl
@@ -8,12 +8,17 @@ my $outfile = shift or die "Usage: $0 infile outfile\n";
 open my $in,  '<', $infile  or die "Can't open $infile: $!";
 open my $out, '>', $outfile or die "Can't open $outfile: $!";
 
+my $L1 = "string1";
+my $R1 = "string2";
+
 while (my $line = <$in>) {
     # Replace .lib ... TT → .lib ... FF
-    $line =~ s/(\.lib\s+'[^']+'\s*)TT(\s*)$/${1}FF${2}/;
+    # $line =~ s/(\.lib\s+'[^']+'\s*)TT(\s*)$/${1}FF${2}/;
 
     # Replace .temp 25 → .temp 50
-    $line =~ s/(\.temp\s+)25(\s*)$/${1}50${2}/;
+    # $line =~ s/(\.temp\s+)25(\s*)$/${1}50${2}/;
+
+    $line =~ s/$L1/$R1/g;
 
     print $out $line;
 }
